27bec0f339a9e41d416fb94b6b72f3309aaf12dd
Promise <glelud@gmail.com>
2021-03-29 15:30:08 +0200

    Create eval command

---
+++ src/commands/global/eval.js
@@ -0,0 +1,23 @@
+module.exports = {
+  description: "Run JavaScript code",
+  options: [
+    {
+      type: 3,
+      name: "code",
+      description: "The code you want to run",
+      required: true
+    }
+  ]
+};
+
+module.exports.execute = async (client, interaction, { code }) => {
+  try {
+    let evaled = eval(code);
+    if (typeof evaled != "string") evaled = require("util").inspect(evaled);
+    return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `🆗 Evaluated successfully.\n\`\`\`js\n${evaled}\`\`\`` } } })
+  } catch(e) {
+    let error = e;
+    if (typeof e == "string") error = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `🆘 JavaScript failed.\n\`\`\`fix\n${error}\`\`\`` } } })
+  }
+};

107e8903d78e8fc3f6aac497914882a9741b807a
Promise <glelud@gmail.com>
2021-03-30 14:23:33 +0200

    Access commands

---
+++ src/commands/global/access/list.js
@@ -0,0 +1,26 @@
+module.exports = {
+  description: "List everyone who has been given access",
+  group: "ACCESS"
+};
+
+const { access, emojis } = require("../../../database"), config = require("../../../../config.json");
+
+module.exports.execute = async (client, interaction) => {
+  const users = Object.keys(await access.get()), { staff, owner } = await emojis.get();
+
+  client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { embeds: [{
+    fields: [
+      {
+        name: "Users with access",
+        value: users.map(u => `${staff} <@${u}>`).join("\n"),
+        inline: true
+      },
+      {
+        name: "Owner",
+        value: `${owner} <@${config.owner}>`,
+        inline: true
+      }
+    ],
+    color: config.embedColors.success
+  }]}}});
+};

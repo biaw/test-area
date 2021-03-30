dcd22f87bac203ce94d52e96e966bebbb6e74782
Promise <glelud@gmail.com>
2021-03-30 15:36:59 +0200

    Add option to output eval command in plain text

--- src/commands/global/eval.js
+++ src/commands/global/eval.js
@@ -6,16 +6,21 @@ module.exports = {
       name: "code",
       description: "The code you want to run",
       required: true
+    },
+    {
+      type: 5,
+      name: "plaintext",
+      description: "Output in plain text"
     }
   ],
   group: "OWNER"
 };
 
-module.exports.execute = async (client, interaction, { code }) => {
+module.exports.execute = async (client, interaction, { code, plaintext = false }) => {
   try {
     let evaled = eval(code);
     if (typeof evaled != "string") evaled = require("util").inspect(evaled);
-    return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `🆗 Evaluated successfully.\n\`\`\`js\n${evaled}\`\`\`` } } });
+    return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: plaintext ? evaled : `🆗 Evaluated successfully.\n\`\`\`js\n${evaled}\`\`\`` } } });
   } catch(e) {
     let error = e;
     if (typeof e == "string") error = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));

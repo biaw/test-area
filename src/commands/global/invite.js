693ea367816256f7b9b89769a6c232d638dca49e
Promise <glelud@gmail.com>
2021-03-30 18:04:08 +0200

    Add invite command

---
+++ src/commands/global/invite.js
@@ -0,0 +1,11 @@
+module.exports = {
+  description: "Get an invite. Add the bot's slash commands to any server you'd like to easily manage your test areas.",
+  group: "OWNER"
+};
+
+const { emojis } = require("../../database");
+
+module.exports.execute = async (client, interaction) => {
+  const { link } = await emojis.get();
+  client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `${link} Authorize me in your server: <https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=applications.commands>` } } });
+};

324a5b7f97e3f163339aa219c684a91b0c581ed0
Promise <glelud@gmail.com>
2021-04-04 00:00:38 +0200

    Send invite privately as an ephemeral message

--- src/commands/global/invite.js
+++ src/commands/global/invite.js
@@ -3,4 +3,4 @@ module.exports = {
   group: "OWNER"
 };
 
-module.exports.execute = async (client, interaction, _, { link }) => client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `${link} Authorize me in your server: <https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=applications.commands>` } } });
+module.exports.execute = async (client, interaction, _, { link }) => client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: `${link} Authorize me in your server: <https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=applications.commands>`, flags: 64 } } });

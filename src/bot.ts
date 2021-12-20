36b34efe3a65e3bb0905c715ffa286cf12c7d7e6
Promise <me@promise.solutions>
2021-12-20 10:10:44 +0100

    Add partials so we receive events for non-cached users

--- src/bot.ts
+++ src/bot.ts
@@ -6,6 +6,7 @@ import { join } from "path";
 import { readdir } from "fs";
 
 const client = new Client({
+  partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"],
   userAgentSuffix: [
     `Responsible is ${config.DISCORD_OWNER_ID}`,
     "Source: https://github.com/biaw/test-area",

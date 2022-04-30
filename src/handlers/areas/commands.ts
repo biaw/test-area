5fa26872ae33b9d7f6594a8036a6b33684a60873
Promise <me@promise.solutions>
2022-04-30 16:56:51 +0200

    Disable command permissions temporarily
    ian broke it

--- src/handlers/areas/commands.ts
+++ src/handlers/areas/commands.ts
@@ -1,44 +1,8 @@
-import type { ApplicationCommand, Guild, GuildApplicationCommandPermissionData } from "discord.js";
-import { areaCommands, slashCommandsFolder, userCommandsFolder } from "../interactions";
-import type { Area } from "../../@types/area";
-import type { SlashCommand } from "../../@types/interactions";
-import { areas } from "../../utils/database";
-import { join } from "path";
+import type { Guild } from "discord.js";
+import { areaCommands } from "../interactions";
 
 export function registerCommands(guild: Guild): Promise<boolean> {
   return new Promise(resolve => {
-    guild.commands.set(areaCommands).then(async commands => {
-      resolve(true);
-      const area = await areas.get(guild.id);
-      const fullPermissions = await getPermissions(Array.from(commands.values()), area);
-      guild.commands.permissions.set({ fullPermissions });
-    }).catch(() => resolve(false));
+    guild.commands.set(areaCommands).then(() => resolve(true)).catch(() => resolve(false));
   });
-}
-
-async function getPermissions(commands: Array<ApplicationCommand>, area: Area) {
-  const fullPermissions: Array<GuildApplicationCommandPermissionData> = [];
-
-  for (const command of commands) {
-    if (!command.defaultPermission) {
-      const { areaPermissionLevel }: SlashCommand = (await import(join(command.type === "CHAT_INPUT" ? slashCommandsFolder : userCommandsFolder, command.name))).default;
-      fullPermissions.push({
-        id: command.id,
-        permissions: [
-          {
-            type: "ROLE",
-            id: area.roles.owner,
-            permission: true,
-          },
-          {
-            type: "ROLE",
-            id: area.roles.elevated,
-            permission: areaPermissionLevel === "ELEVATED",
-          },
-        ],
-      });
-    }
-  }
-
-  return fullPermissions;
 }
